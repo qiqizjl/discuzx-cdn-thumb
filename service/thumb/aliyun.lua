3d9899c1ef50bbfbea0f66d88fa605a6822ed922
耐小心 <qiqizjl@qq.com>
2018-12-04 11:08:39 +0800

    fix bug

--- service/thumb/aliyun.lua
+++ service/thumb/aliyun.lua
@@ -2,7 +2,11 @@ local _M = {}
 
 function _M:thumb(image_url, width)
     local url =  image_url ..  "?x-oss-process=image/resize,m_mfit,w_" .. width
-    local match,err = ngx.re.match(ngx.req.get_headers()["accept"],"webp")
+    local accept = ngx.req.get_headers()["accept"]
+    if accept == nil then
+        accept = ""
+    end
+    local match,err = ngx.re.match(accept,"webp")
     if match  then
         url  = url .. "/format,webp"
     end
